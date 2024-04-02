9769519e116ffbcedae4b90e79874ab49fa4fa4c
Sammy-ch <sammystcherubin@gmail.com>
2024-04-02 11:35:28 +0200

    Added Ant Design packages

--- web/src/components/DashboardHeader/DashboardHeader.tsx
+++ web/src/components/DashboardHeader/DashboardHeader.tsx
@@ -1,14 +1,15 @@
+import UserOutlined from '@ant-design/icons'
+import {Avatar} from 'antd'
 import { Bell } from 'lucide-react'
-import { Avatar } from 'primereact/avatar'
 
 import logo from './MD.jpg'
 const DashboardHeader = () => {
   return (
     <div className="flex h-[80px] w-full items-center justify-between rounded-[5px]">
       <h1 className="text-[30px] font-bold ">Overview</h1>
-      <div className='flex items-center gap-5'>
-        <Avatar image={logo} size="normal" shape="circle" />
+      <div className="flex items-center gap-5">
         <Bell />
+        <Avatar size={45} icon={<UserOutlined />} src={logo} />
       </div>
     </div>
   )
--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
-import { Chart } from 'primereact/chart'
 
 import { Metadata } from '@redwoodjs/web'
 
@@ -8,77 +7,11 @@ import DashboardHeader from 'src/components/DashboardHeader/DashboardHeader'
 
 const HomePage = () => {
 
-  const [chartData, setChartData] = useState({})
-  const [chartOptions, setChartOptions] = useState({})
-
-  useEffect(() => {
-    const documentStyle = getComputedStyle(document.documentElement)
-    const textColor = documentStyle.getPropertyValue('--text-color')
-    const textColorSecondary = documentStyle.getPropertyValue(
-      '--text-color-secondary'
-    )
-    const surfaceBorder = documentStyle.getPropertyValue('--surface-border')
-    const data = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-        {
-          label: 'My First dataset',
-          backgroundColor: documentStyle.getPropertyValue('--blue-500'),
-          borderColor: documentStyle.getPropertyValue('--blue-500'),
-          data: [65, 59, 80, 81, 56, 55, 40],
-        },
-        {
-          label: 'My Second dataset',
-          backgroundColor: documentStyle.getPropertyValue('--pink-500'),
-          borderColor: documentStyle.getPropertyValue('--pink-500'),
-          data: [28, 48, 40, 19, 86, 27, 90],
-        },
-      ],
-    }
-    const options = {
-      maintainAspectRatio: false,
-      aspectRatio: 0.8,
-      plugins: {
-        legend: {
-          labels: {
-            fontColor: textColor,
-          },
-        },
-      },
-      scales: {
-        x: {
-          ticks: {
-            color: textColorSecondary,
-            font: {
-              weight: 500,
-            },
-          },
-          grid: {
-            display: false,
-            drawBorder: false,
-          },
-        },
-        y: {
-          ticks: {
-            color: textColorSecondary,
-          },
-          grid: {
-            color: surfaceBorder,
-            drawBorder: false,
-          },
-        },
-      },
-    }
-
-    setChartData(data)
-    setChartOptions(options)
-  }, [])
 
   return (
     <main className="w-full bg-[#F5F5F5] px-5 pt-2">
       <Metadata title="Home" description="Palm HR Dashboard" />
       <DashboardHeader />
-      <Chart type="bar" data={chartData} options={chartOptions} />
     </main>
   )
 }
