52b0dbccceeff1b9e348743354a50527ca96d671
Sammy-ch <sammystcherubin@gmail.com>
2024-04-18 10:52:55 +0200

    More dashboard layout updates

--- web/src/pages/DashboardPage/DashboardPage.tsx
+++ web/src/pages/DashboardPage/DashboardPage.tsx
@@ -5,15 +5,12 @@ import { Metadata } from '@redwoodjs/web'
 import AttendanceActivityTable from 'src/components/AttendanceActivityTable/AttendanceActivityTable'
 import AttendanceChartCard from 'src/components/AttendanceChartCard/AttendanceChartCard'
 import AttendancePieChart from 'src/components/AttendancePieChart/AttendancePieChart'
-import DashboardHeader from 'src/components/DashboardHeader/DashboardHeader'
 import KpiCard from 'src/components/KpiCard/KpiCard'
 
 const DashboardPage = () => {
   return (
-    <main className="content h-full w-full  gap-5 px-5 py-2 ">
+    <main className="content h-full w-full  gap-5  ">
       <Metadata title="Dashboard" description="Access your Dashboard" />
-
-      <DashboardHeader />
       <section className=" flex h-full flex-col gap-5 ">
         <section className="flex flex-col">
           <div className="flex gap-5 rounded-md ">
@@ -24,15 +21,15 @@ const DashboardPage = () => {
           </div>
         </section>
         <section className="flex w-full gap-5">
-          <div className="h-[400px] rounded-md border  shadow-md">
+          <div className="rounded-md   border  shadow-md">
             <AttendanceChartCard />
           </div>
-          <div className="w-[400px] rounded-md shadow-md">
+          {/* <div className="rounded-md shadow-md">
             <AttendancePieChart />
-          </div>
+          </div> */}
         </section>
         <section className="flex h-full w-full grid-flow-col gap-5">
-          <div className="col-span-3 w-full rounded-md p-5 shadow-md">
+          <div className="col-span-3 w-full rounded-md ">
             <AttendanceActivityTable />
           </div>
         </section>
