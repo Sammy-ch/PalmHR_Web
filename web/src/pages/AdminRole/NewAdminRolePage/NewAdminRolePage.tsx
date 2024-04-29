0d40065a228ad4f0479edcc328f34677efa2dc6c
Sammy-ch <sammystcherubin@gmail.com>
2024-04-29 12:51:44 +0200

    Updated DB Schema and SDL services

--- web/src/pages/AdminRole/NewAdminRolePage/NewAdminRolePage.tsx
+++
@@ -1,7 +0,0 @@
-import NewAdminRole from 'src/components/AdminRole/NewAdminRole'
-
-const NewAdminRolePage = () => {
-  return <NewAdminRole />
-}
-
-export default NewAdminRolePage
--- web/src/pages/UserProfile/UserProfilesPage/UserProfilesPage.tsx
+++
@@ -1,7 +0,0 @@
-import UserProfilesCell from 'src/components/UserProfile/UserProfilesCell'
-
-const UserProfilesPage = () => {
-  return <UserProfilesCell />
-}
-
-export default UserProfilesPage
