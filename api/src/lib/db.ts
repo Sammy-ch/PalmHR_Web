1b28bc44b11fcf1dd80922165e9895bee599e76c
Sammy-ch <sammystcherubin@gmail.com>
2024-04-30 13:03:06 +0200

    updated prisma db client

--- api/src/lib/db.ts
+++ api/src/lib/db.ts
@@ -1,25 +1,28 @@
 // See https://www.prisma.io/docs/reference/tools-and-interfaces/prisma-client/constructor
 // for options.
 
-import { createClient } from '@libsql/client'
-import { PrismaLibSQL } from '@prisma/adapter-libsql'
-import { PrismaClient } from '@prisma/client'
+import { createClient } from '@supabase/supabase-js'
 
 import { handlePrismaLogging } from '@redwoodjs/api/logger'
 
 import { logger } from './logger'
 
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+)
+
 /*
  * Instance of the Prisma Client
  */
 
-const libsql = createClient({
-  url: `${process.env.TURSO_DATABASE_URL}`,
-  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
-})
+// const libsql = createClient({
+//   url: `${process.env.TURSO_DATABASE_URL}`,
+//   authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+// })
 
-const adapter = new PrismaLibSQL(libsql)
-export const db = new PrismaClient({ adapter })
+// const adapter = new PrismaLibSQL(libsql)
+export const db = supabase
 
 handlePrismaLogging({
   db,
