a1a8a88b1942be54fa20c107fe9bedb99afc6b9b
Sammy-ch <sammystcherubin@gmail.com>
2024-05-07 16:41:16 +0200

    minor update #5

--- api/src/functions/checkingRequestHook/checkingRequestHook.ts
+++ api/src/functions/checkingRequestHook/checkingRequestHook.ts
@@ -1,33 +1,22 @@
 import type { APIGatewayEvent, Context } from 'aws-lambda'
 
 import { logger } from 'src/lib/logger'
-
 export const handler = async (event: APIGatewayEvent, _context: Context) => {
   logger.info(`${event.httpMethod} ${event.path}: checkingRequestHook function`)
 
   try {
-    let body = null
+    const body = event.body
 
-    // Parse the event body if it exists
-    if (event.body) {
-      body = JSON.parse(event.body)
-    } else {
-      throw new Error('Request body is empty')
-    }
-
-    // Access the fields from the body object
-    const {
-      id,
-      employee_id: employeeId,
-      checking_date: checkingDate,
-      checking_time: checkingTime,
-      checking_type: checkingType,
-      checking_status: checkingStatus,
-    } = body
+    // if (body.table !== 'CheckingRequest') {
+    //   return {
+    //     statusCode: 400,
+    //     body: JSON.stringify({ error: 'Invalid table' }),
+    //   }
+    // }
 
     // Process the checking request data here
     // For example, save it to your database or perform any other necessary actions
-console.log(body)
+    console.log(body)
     return {
       statusCode: 201,
       headers: {
