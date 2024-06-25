20661ca203557bc45f471c0402144cb3fccccb16
Sammy-ch <sammystcherubin@gmail.com>
2024-06-25 14:15:50 +0200

    Updated Login Page UI

--- web/src/pages/LoginPage/LoginPage.tsx
+++ web/src/pages/LoginPage/LoginPage.tsx
@@ -4,6 +4,7 @@ import {
   Form,
   TextField,
   PasswordField,
+  Label,
   Submit,
   FieldError,
 } from '@redwoodjs/forms'
@@ -11,8 +12,6 @@ import { Link, navigate, routes } from '@redwoodjs/router'
 import { toast } from '@redwoodjs/web/toast'
 
 import { useAuth } from 'src/auth'
-import { Input } from 'src/components/ui/input'
-import { Label } from 'src/components/ui/label'
 
 const LoginPage = () => {
   const { client, logIn } = useAuth()
@@ -47,7 +46,9 @@ const LoginPage = () => {
       toast(response.error.message)
     } else {
       toast.success('Welcome back!')
-      navigate(routes.dashboard({ id: userSession }))
+      if (userSession) {
+        navigate(routes.dashboard({ id: userSession }))
+      }
     }
   }
 
@@ -55,40 +56,72 @@ const LoginPage = () => {
     <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
-          <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-bold">Login</h1>
-            <p className="text-balance text-muted-foreground">
+          <div className="grid gap-2 text-left">
+            <div>
+              <img
+                alt="placeholder"
+                src="/pages/LoginPage/logo.png"
+                className="h-[140px] w-[200px]  dark:brightness-[0.2] dark:grayscale"
+              />
+            </div>
+            {/* <h1 className="text-3xl font-bold">Login to PALM HR</h1> */}
+            <p className="text-muted-foreground">
               Enter your email below to login to your dashboard
             </p>
           </div>
-          <Form onSubmit={onSubmit} className="">
-            <div className="grid gap-4">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  name="email"
-                  ref={emailRef}
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <Link
-                    to="/forgot-password"
-                    className="ml-auto inline-block text-sm underline"
-                  >
-                    Forgot your password?
-                  </Link>
-                </div>
-                <Input id="password" name="password" type="password" required />
-              </div>
-              <Submit className="w-full rounded-md bg-black p-2 text-white">
-                Login
-              </Submit>
+          <Form onSubmit={onSubmit} className="rw-form-wrapper">
+            <Label
+              name="email"
+              className="rw-label"
+              errorClassName="rw-label rw-label-error"
+            >
+              Email
+            </Label>
+            <TextField
+              name="email"
+              className="rw-input"
+              errorClassName="rw-input rw-input-error"
+              ref={emailRef}
+              validation={{
+                required: {
+                  value: true,
+                  message: 'Email is required',
+                },
+              }}
+            />
+
+            <FieldError name="email" className="rw-field-error" />
+
+            <Label
+              name="password"
+              className="rw-label"
+              errorClassName="rw-label rw-label-error"
+            >
+              Password
+            </Label>
+            <PasswordField
+              name="password"
+              className="rw-input"
+              errorClassName="rw-input rw-input-error"
+              autoComplete="current-password"
+              validation={{
+                required: {
+                  value: true,
+                  message: 'Password is required',
+                },
+              }}
+            />
+
+            <div className="rw-forgot-link">
+              <Link to={routes.forgotPassword()} className="rw-forgot-link">
+                Forgot Password?
+              </Link>
+            </div>
+
+            <FieldError name="password" className="rw-field-error" />
+
+            <div className="rw-button-group">
+              <Submit className="rw-button rw-button-blue">Login</Submit>
             </div>
           </Form>
         </div>
@@ -96,9 +129,7 @@ const LoginPage = () => {
       <div className="hidden bg-muted lg:block">
         <img
           alt="placeholder"
-          src="/placeholder.svg"
-          width="1920"
-          height="1080"
+          src="/pages/LoginPage/login.jpeg"
           className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
       </div>
