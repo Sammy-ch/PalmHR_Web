fd25a9ce7bd8aeb5e12937b83ad730d0e774a42e
Sammy-ch <sammystcherubin@gmail.com>
2024-07-15 16:54:07 +0200

    Migrated CheckingRequestQueues to Kysely DB

--- api/src/services/checkingRequestQueues/checkingRequestQueues.ts
+++ api/src/services/checkingRequestQueues/checkingRequestQueues.ts
@@ -5,7 +5,19 @@ import type {
 } from 'types/graphql'
 
 import { db } from 'src/lib/db'
+import { kyselyDB } from 'src/lib/kysely'
 
+const formatDatetime = (dateTime: string | Date): Date => {
+  if (typeof dateTime === 'string' && /^\d{2}:\d{2}:\d{2}$/.test(dateTime)) {
+    // If the input is a time string (e.g., "08:22:00"), prepend a default date
+    dateTime = `1970-01-01T${dateTime}Z`
+  }
+  const date = new Date(dateTime)
+  if (isNaN(date.getTime())) {
+    throw new Error(`Invalid date: ${dateTime}`)
+  }
+  return date
+}
 export const checkingRequestQueues: QueryResolvers['checkingRequestQueues'] =
   () => {
     return db.checkingRequestQueue.findMany()
@@ -20,17 +32,23 @@ export const checkingRequestQueue: QueryResolvers['checkingRequestQueue'] = ({
 }
 
 export const checkingRequestQueuesByOrganization: QueryResolvers['checkingRequestQueuesByOrganization'] =
-  ({ organizationId }) => {
-    return db.checkingRequestQueue.findMany({
-      where: {
-        employee: {
-          org_id: organizationId,
-        },
-      },
-      include: {
-        employee: true,
-      },
-    })
+  async ({ organizationId }) => {
+    const queues = await kyselyDB
+      .selectFrom('CheckingRequestQueue')
+      .innerJoin(
+        'EmployeeProfile',
+        'CheckingRequestQueue.employee_id',
+        'EmployeeProfile.profile_id'
+      )
+      .selectAll('CheckingRequestQueue')
+      .selectAll('EmployeeProfile')
+      .where('EmployeeProfile.org_id', '=', organizationId)
+      .execute()
+
+    return queues.map((queue) => ({
+      ...queue,
+      checking_time: formatDatetime(queue.checking_time),
+    }))
   }
 
 export const createCheckingRequestQueue: MutationResolvers['createCheckingRequestQueue'] =
